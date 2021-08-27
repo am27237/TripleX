37177a15fffbf0ea429c759e18dcdda548b4b7a0
Alex Miranda <am27237@gmail.com>
2021-08-27 20:54:30 +0800

    Introducing function and use \n escape sequence to create new line

--- TripleX.cpp
+++ TripleX.cpp
@@ -1,12 +1,18 @@
 #include <iostream>
 
-int main()
+void GameIntroduction()
 {
     // print welcome message to the terminal
     std::cout << std::endl;
     std::cout << "You are a secret agent breaking into a secure server room";   //print out the string
     std::cout << std::endl;                                                     //create new line
-    std::cout << "You need to enter the correct codes to continue" << std::endl;
+    std::cout << "You need to enter the correct codes to continue\n";  
+    //Note: we can use \n instead of std::endl. \n is called escape sequence in strings. Cannot be used in variables
+}
+
+void PlayGame()
+{
+    GameIntroduction();
 
     //To follow unreal engine 4 naming convention, variable declaration must start with a capital letter. Dont use "_" in between.
     const int CodeA = 4;    
@@ -19,7 +25,7 @@ int main()
 
     // print sum and product the terminal
     std::cout << std::endl;
-    std::cout << "+ There are three numbers in the code" << std::endl;
+    std::cout << "+ There are three numbers in the code\n";
     std::cout << "+ The codes adds up to " << CodeSum << std::endl;            //display the sum and create new line
     std::cout << "+ The codes multiply to give " << CodeProduct << std::endl;            // display the product
     std::cout << std::endl;
@@ -35,8 +41,7 @@ int main()
     std::cin >> GuessC;
     std::cout << std::endl;
 
-    std::cout <<"You entered: " << GuessA << GuessB << GuessC;
-    std::cout << std::endl;
+    std::cout <<"You entered: " << GuessA << GuessB << GuessC << std::endl;
 
     //initializing variables and performing math operations
     int GuessSum = GuessA + GuessB + GuessC;
@@ -45,12 +50,16 @@ int main()
 
     if (GuessSum==CodeSum && GuessProduct==CodeProduct)
     {
-        std::cout << "Your guess is correct, You win!";
+        std::cout << "Your guess is correct, You win!\n";
     }
     else
     {
-        std::cout << "You guess was wrong, you lose!";
+        std::cout << "You guess was wrong, you lose!\n";
     }
+}
 
+int main()
+{
+    PlayGame();
     return 0;
 }
